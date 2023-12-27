9acd750600004a50ab48f7610fb291217476f3d5
DAVID CERNY <david.cerny.021@pslib.cz>
2023-12-27 13:07:00 +0100

    you can actually restart the game now

--- ProjectGamebook/Pages/GameEnd.cshtml.cs
+++ ProjectGamebook/Pages/GameEnd.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectGamebook.Models;
+using ProjectGamebook.Services;
 
 namespace ProjectGamebook.Pages
 {
     public class GameEndModel : PageModel
     {
+        private const string KEY = "SWEETREVENGE";
+        private readonly ISessionStorage<GameState> _ss;
+
+        public GameState GS { get; set; }
+
+        public GameEndModel(ISessionStorage<GameState> ss)
+        {
+            _ss = ss;
+
+            GS = _ss.LoadOrCreate(KEY);
+            GS.PreviousLocation = 666666;
+            _ss.Save(KEY, GS);
+        }
+
         public void OnGet()
         {
+            
         }
     }
 }
--- ProjectGamebook/Pages/GameOver.cshtml.cs
+++ ProjectGamebook/Pages/GameOver.cshtml.cs
@@ -1,10 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectGamebook.Models;
+using ProjectGamebook.Services;
 
 namespace ProjectGamebook.Pages
 {
     public class GameOverModel : PageModel
     {
+        private const string KEY = "SWEETREVENGE";
+        private readonly ISessionStorage<GameState> _ss;
+
+        public GameState GS { get; set; }
+
+        public GameOverModel(ISessionStorage<GameState> ss)
+        {
+            _ss = ss;
+
+            GS = _ss.LoadOrCreate(KEY);
+            GS.PreviousLocation = 666666;
+            _ss.Save(KEY, GS);
+        }
+
         public void OnGet()
         {
         }
