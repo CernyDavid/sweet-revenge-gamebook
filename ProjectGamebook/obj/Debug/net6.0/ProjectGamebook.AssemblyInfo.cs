9b5821d00635284993b14bb01df3d8d86797c42c
DAVID CERNY <david.cerny.021@pslib.cz>
2024-01-11 22:10:01 +0100

    added some quick fixes to the css code so that it looks like it was made by someone who actually has some understanding of css

--- ProjectGamebook/obj/Debug/net6.0/ProjectGamebook.AssemblyInfo.cs
+++ ProjectGamebook/obj/Debug/net6.0/ProjectGamebook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de6efc9755815fefa70e6aaaff56d5b24aadd67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
