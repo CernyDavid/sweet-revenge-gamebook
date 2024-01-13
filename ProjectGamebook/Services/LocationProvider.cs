a7b18e5a74fc1ac295ca7b4c211872c4777f143b
DAVID CERNY <david.cerny.021@pslib.cz>
2024-01-13 13:37:49 +0100

    added loading from json (i have no idea who the heck is j and why does he have a son)

--- ProjectGamebook/Services/LocationProvider.cs
+++ ProjectGamebook/Services/LocationProvider.cs
@@ -4,15 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace ProjectGamebook.Services
 {
     public class LocationProvider : ILocationProvider
     {
 
-        private static readonly Dictionary<int, Location> _locations = new()
-        {
-            {0, new Location(new List<string> { "This is it. The Labyrinth Castle.", "Looks like some monster came to welcome you. Get rid of it before it's too late.", "(You have two ways of attacking. Regular attacks are sure to hit and deal regular amount of damage. You can also attempt a critical hit. Critical hit won't always succeed but deals great damage if it lands.)" }, "~/imgs/bgs/one_door_bg2.jpeg" ) },
+        private static readonly Dictionary<int, Location> _locations = LoadLocations();
+
+        /*            {0, new Location(new List<string> { "This is it. The Labyrinth Castle.", "Looks like some monster came to welcome you. Get rid of it before it's too late.", "(You have two ways of attacking. Regular attacks are sure to hit and deal regular amount of damage. You can also attempt a critical hit. Critical hit won't always succeed but deals great damage if it lands.)" }, "~/imgs/bgs/one_door_bg2.jpeg" ) },
             {1, new Location(new List<string> { "You may be wondering, why is it called the Labyrinth Castle? Well, you see, this place is like a labyrinth.", "You have two paths ahead of you. There is no map available, so just trust your gut and choose. Which way will you go?" }, "~/imgs/bgs/two_door_bg1.jpeg") },
             {2, new Location(new List<string> { "What is the best way to kill monsters made of sweets? Using something salty, of course.", "Luckily for you, you found a very salty and very deadly weapon. Pick it up and continue." }, "~/imgs/bgs/one_door_bg1.jpeg") },
             {3, new Location(new List<string> { "Oh, a monster. You know what to do." }, "~/imgs/bgs/one_door_bg2.jpeg" ) },
@@ -24,27 +26,9 @@ namespace ProjectGamebook.Services
             {9, new Location(new List<string> { "This fucker really thinks he stands a chance, huh?", "Show him who’s the boss here." }, "~/imgs/bgs/one_door_bg1.jpeg") },
             {10, new Location(new List<string> { "This fucker really thinks he stands a chance, huh?", "Show him who’s the boss here." }, "~/imgs/bgs/one_door_bg1.jpeg") },
             {11, new Location(new List<string> { "Would you look at that! Food!", "Eating something salty will lower your diabetes level a bit. Try it out." }, "~/imgs/bgs/one_door_bg2.jpeg") },
-            {30, new Location(new List<string> { "Well, you sure ain’t a pushover. But neither are they, so don’t let it get to your head, ‘kay?", "Also, you got a new weapon. Isn’t that great?" }, "~/imgs/bg1.jpg") },
-        };
+            {30, new Location(new List<string> { "Well, you sure ain’t a pushover. But neither are they, so don’t let it get to your head, ‘kay?", "Also, you got a new weapon. Isn’t that great?" }, "~/imgs/bg1.jpg") },*/
 
-        private readonly List<Connection> _map = new()
-        {
-            new Connection(0,1,"40%","35%","30%","50%", IsFightFinished, null),
-            new Connection(1,2,"10%","0","30%","80%", null, null),
-            new Connection(1,3,"10%","70%","30%","80%", null, null),
-            new Connection(2,4,"50%","40%","20%","40%", null, null),
-            new Connection(3,5,"40%","35%","30%","50%", IsFightFinished, null),
-            new Connection(5,6,"50%","40%","20%","40%", null, null),
-            new Connection(4,6,"40%","35%","30%","50%", null, null),
-            new Connection(6,7,"50%","40%","20%","40%", null, null),
-            new Connection(7,8,"10%","0","30%","80%", null, null),
-            new Connection(7,9,"10%","70%","30%","80%", null, null),
-            new Connection(9,11,"50%","40%","20%","40%", IsFightFinished, null),
-            new Connection(8,10,"40%","35%","30%","50%", null, null),
-            new Connection(10,30,"50%","40%","20%","40%", null, "ChocoBossFight"),
-            new Connection(11,30,"40%","35%","30%","50%", null, "ChocoBossFight")
-
-        };
+        private readonly List<Connection> _map = LoadConnections();
 
         public static bool IsFightFinished(int id)
         {
@@ -103,6 +87,75 @@ namespace ProjectGamebook.Services
                 throw new InvalidNavigationException("Invalid move detected.");
             }
             throw new InvalidNavigationException("Invalid move detected.");
+        }
+
+        private static Dictionary<int, Location> LoadLocations()
+        {
+            string json = "locations.json";
+
+            try
+            {
+                string jsonContent = File.ReadAllText(json);
+                List<LocationData> locationDataList = JsonSerializer.Deserialize<List<LocationData>>(jsonContent);
+
+                Dictionary<int, Location> locations = new Dictionary<int, Location>();
+
+                foreach (LocationData locationData in locationDataList)
+                {
+                    Location location = new Location(locationData.Texts, locationData.ImageUrl);
+                    locations.Add(locationData.Id, location);
+                }
+
+                return locations;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading locations: " + ex.Message);
+                return new Dictionary<int, Location>();
+            }
+        }
+        public class LocationData
+        {
+            public int Id { get; set; }
+            public List<string> Texts { get; set; }
+            public string ImageUrl { get; set; }
+        }
+        private static List<Connection> LoadConnections()
+        {
+            string json = "connections.json";
+
+            try
+            {
+                string jsonContent = File.ReadAllText(json);
+                List<ConnectionData> connectionDataList = JsonSerializer.Deserialize<List<ConnectionData>>(jsonContent);
+
+                List<Connection> connections = new List<Connection>();
+
+                foreach (ConnectionData connectionData in connectionDataList)
+                {
+                    Connection connection = new Connection(connectionData.From, connectionData.To, connectionData.Top, connectionData.Left, connectionData.Width, connectionData.Height, null, connectionData.SpecialPage);
+                    connections.Add(connection);
+                }
+
+                return connections;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading connections: " + ex.Message);
+                return new List<Connection>();
+            }
+        }
+        public class ConnectionData
+        {
+            public int From { get; set; }
+            public int To { get; set; }
+            public string Top { get; set; }
+            public string Left { get; set; }
+            public string Width { get; set; }
+            public string Height { get; set; }
+            public bool? Condition { get; set; }
+            public string SpecialPage { get; set; }
+
         }
     }
 }
--- ProjectGamebook/obj/Debug/net6.0/ProjectGamebook.AssemblyInfo.cs
+++ ProjectGamebook/obj/Debug/net6.0/ProjectGamebook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eba34ecf482fc15c75720019d99c8ea509669e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13776d4dca5de2cb512e1556e62f5d2088e694f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectGamebook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
